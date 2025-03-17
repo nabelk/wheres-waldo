aea8d1013e4e5db6f0e75e7a6d640e5bb75a000b
Nabil Khalid <holanabilkhalid@gmail.com>
2025-03-17 23:41:08 +0800

    feat: game states into game slice

--- lib/features/game/gameSlice.ts
+++ lib/features/game/gameSlice.ts
@@ -2,10 +2,18 @@ import { createSlice } from "@reduxjs/toolkit";
 
 export interface GameState {
   timer: number;
+  clickPosition: { x: number; y: number };
+  isCharacterFound: { name: string; found: boolean }[];
 }
 
 const initialState: GameState = {
   timer: 0,
+  clickPosition: { x: 0, y: 0 },
+  isCharacterFound: [
+    { name: "Waldo", found: false },
+    { name: "Wizard", found: false },
+    { name: "Odlaw", found: false },
+  ],
 };
 
 export const gameSlice = createSlice({
@@ -18,9 +26,16 @@ export const gameSlice = createSlice({
     reset: (state) => {
       state.timer = 0;
     },
+    setClickPosition: (state, action) => {
+      state.clickPosition = action.payload;
+    },
+    setIsCharacterFound: (state, action) => {
+      state.isCharacterFound = action.payload;
+    },
   },
 });
 
-export const { increment, reset } = gameSlice.actions;
+export const { increment, reset, setClickPosition, setIsCharacterFound } =
+  gameSlice.actions;
 
 export default gameSlice.reducer;
