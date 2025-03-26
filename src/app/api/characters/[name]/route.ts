dc9eac02946049fe5e1b3a55b4955d203480a69a
Nabil Khalid <holanabilkhalid@gmail.com>
2025-03-27 00:12:15 +0800

    fix: secure a bit of checking chrac pos

--- src/app/api/characters/[name]/route.ts
+++ src/app/api/characters/[name]/route.ts
@@ -1,10 +1,18 @@
 import { NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
+import { redirect } from "next/navigation";
 
 export async function GET(
   req: Request,
   { params }: { params: Promise<{ name: string }> }
 ) {
+  const apiKey = process.env.NEXT_PUBLIC_API_KEY;
+  const requestApiKey = req.headers.get("x-api-key");
+
+  if (!requestApiKey || requestApiKey !== apiKey) {
+    return redirect("/?error=unauthorized");
+  }
+
   const { name } = await params;
   const character = await prisma.characterPosition.findFirst({
     where: {
