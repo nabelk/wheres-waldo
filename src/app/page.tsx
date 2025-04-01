32a7a2d21a0f2bc1edc4dcf810fbdda53e58fce6
Nabil Khalid <holanabilkhalid@gmail.com>
2025-04-02 07:47:48 +0800

    feat: show err toast if client directly access game api & automatically open leaderboard modal from submit page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,21 @@
 
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
-import { reset, setIsCharacterFound } from "@/lib/features/game/gameSlice";
+import {
+  reset,
+  setIsCharacterFound,
+  setClickPosition,
+} from "@/lib/features/game/gameSlice";
 import Link from "next/link";
+import Leaderboard from "./component/leaderboard";
+import { useSearchParams } from "next/navigation";
+import { toast } from "sonner";
 
 export default function WaldoHome() {
   const dispatch = useDispatch();
+  const searchParams = useSearchParams();
+  const leaderboard = searchParams.get("leaderboard");
+  const error = searchParams.get("error");
 
   useEffect(() => {
     dispatch(reset());
@@ -17,22 +27,42 @@ export default function WaldoHome() {
         { name: "Odlaw", found: false },
       ])
     );
-  }, [dispatch]);
+    dispatch(setClickPosition({ x: 0, y: 0 }));
+  }, [dispatch, searchParams]);
+
+  useEffect(() => {
+    if (error) {
+      toast.error("Please don't cheat", {
+        style: {
+          color: "#f7adee",
+          backgroundColor: "#440829",
+        },
+      });
+      const url = new URL(window.location.href);
+      if (url.searchParams.has("error")) {
+        url.searchParams.delete("error");
+        window.history.replaceState(null, "", url.toString());
+      }
+    }
+  }, [searchParams]);
 
   return (
     <div className="flex text-[#f7adee] flex-col items-center justify-center min-h-screen bg-[#3434ad] p-4">
       <h1 className="text-4xl md:text-6xl font-bold  mb-6 text-center">
-        Where's Waldo?
+        Where&apos;s Waldo?
       </h1>
       <p className="text-lg md:text-xl mb-8 text-center">
         Find the characters as fast as you can!
       </p>
-      <Link
-        href="/game"
-        className="px-6 py-3 bg-[#440829] text-lg md:text-xl font-semibold rounded-lg shadow-md hover:bg-[#440829dc] transition"
-      >
-        Start Game
-      </Link>
+      <div className="flex gap-2 flex-wrap justify-center">
+        <Link
+          href="/game"
+          className="px-6 py-3 bg-[#440829] text-lg md:text-xl font-semibold rounded-lg shadow-md hover:bg-[#440829dc] transition"
+        >
+          Start Game
+        </Link>
+        <Leaderboard modalOpen={leaderboard ? true : false} />
+      </div>
     </div>
   );
 }
