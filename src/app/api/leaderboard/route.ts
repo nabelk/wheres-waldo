b4616025d492cf54a580949bbbcd13e8be6ede33
Nabil Khalid <holanabilkhalid@gmail.com>
2025-04-01 23:52:48 +0800

    feat: secure api a bit with api key

--- src/app/api/leaderboard/route.ts
+++ src/app/api/leaderboard/route.ts
@@ -1,7 +1,13 @@
 import { NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
 
-export async function GET() {
+export async function GET(req: Request) {
+  const apiKey = process.env.NEXT_PUBLIC_API_KEY;
+  const requestApiKey = req.headers.get("x-api-key");
+
+  if (!requestApiKey || requestApiKey !== apiKey) {
+    return new NextResponse("Unauthorized", { status: 401 });
+  }
   const leaderboard = await prisma.leaderboard.findMany({
     orderBy: {
       time: "asc",
--- src/app/api/submit/route.ts
+++ src/app/api/submit/route.ts
@@ -2,6 +2,12 @@ import { NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
 
 export async function POST(req: Request) {
+  const apiKey = process.env.NEXT_PUBLIC_API_KEY;
+  const requestApiKey = req.headers.get("x-api-key");
+
+  if (!requestApiKey || requestApiKey !== apiKey) {
+    return new NextResponse("Unauthorized", { status: 401 });
+  }
   const { time, name } = await req.json();
   const score = await prisma.leaderboard.create({
     data: {
